a5d895b819c7ac0b4864184176d833c69409fcb7
ACSouzaJr <junioracs2000@hotmail.com>
2017-09-15 10:26:42 -0300

    passa no teste de numero negativo

--- Projeto 1/string_soma.c
+++ Projeto 1/string_soma.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include "string_soma.h"
 
+int IsNegative(int Num);
+int HighValue(int Num);
+
 int soma_string(const char* string_entrada){
 
     //separa a string por delimitadores, a cada separaçao soma numero em soma.
@@ -15,11 +18,27 @@ int soma_string(const char* string_entrada){
 
     while(string != NULL){
 
+        if (IsNegative(atoi(string))) return -1;
+        //if (!HighValue(atoi(string)))
+
+
         soma += atoi(string);
         //puts(string);
         string = strtok(NULL, ", ");
+
+
 
     }
 
     return soma;
 }
+
+int IsNegative(int Num){
+
+    return Num < 0 ? 1:0;
+}
+
+/*int HighValue(int Num){
+
+    return Num > 1000 ? 1:0;
+}*/
