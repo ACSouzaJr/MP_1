2f93cc67a2e330158a6a651d5d05f553c16ba1c0
ACSouzaJr <junioracs2000@hotmail.com>
2017-09-16 11:45:45 -0300

    passa teste delimitador repetido

--- Projeto 1/Test/testa_string_soma.c
+++ Projeto 1/Test/testa_string_soma.c
@@ -1,52 +1,61 @@
 #include <gtest/gtest.h>
 #include "string_soma.h"
 
-TEST (Soma, PosNum){
-    EXPECT_EQ (5, soma_string("3,2\n"));
-    EXPECT_EQ (3, soma_string("1,2\n"));
-    EXPECT_EQ (6, soma_string("3,2,1\n"));
-}
+    TEST (Soma, PosNum){
+        EXPECT_EQ (5, soma_string("3,2\n"));
+        EXPECT_EQ (3, soma_string("1,2\n"));
+        EXPECT_EQ (6, soma_string("3,2,1\n"));
+    }
 
-TEST (Valido, LimiteLinha){
-    EXPECT_EQ (3, soma_string("\n1,2\n"));
-    EXPECT_EQ (6, soma_string("1,2,3\n"));
-    EXPECT_EQ (10, soma_string("1,2,3\n,4\n"));
-    EXPECT_EQ (6, soma_string("1\n,2\n,3\n"));
-}
+    TEST (Valido, LimiteLinha){
+        EXPECT_EQ (3, soma_string("\n1,2\n"));
+        EXPECT_EQ (6, soma_string("1,2,3\n"));
+        EXPECT_EQ (10, soma_string("1,2,3\n,4\n"));
+        EXPECT_EQ (6, soma_string("1\n,2\n,3\n"));
+    }
 
-TEST(Soma, ManyLines){
-    EXPECT_EQ (3, soma_string("1\n,2\n"));
-    EXPECT_EQ (3, soma_string("1\n\n\n,2\n"));
-    EXPECT_EQ (3, soma_string("1\n\n\n,\n\n\n2\n"));
-}
+    TEST(Soma, ManyLines){
+        EXPECT_EQ (3, soma_string("1\n,2\n"));
+        EXPECT_EQ (3, soma_string("1\n\n\n,2\n"));
+        EXPECT_EQ (3, soma_string("1\n\n\n,\n\n\n2\n"));
+    }
 
-TEST (Invalido, NumNegativo){
-    EXPECT_EQ (-1, soma_string("1,2,-3\n"));
-    EXPECT_EQ (-1, soma_string("-1,2\n"));
-    EXPECT_EQ (-1, soma_string("-1,-2\n"));
-    EXPECT_EQ (-1, soma_string("1,-2\n"));
-    EXPECT_EQ (-1, soma_string("1\n,-2\n"));
-}
+    TEST (Invalido, NumNegativo){
+        EXPECT_EQ (-1, soma_string("1,2,-3\n"));
+        EXPECT_EQ (-1, soma_string("-1,2\n"));
+        EXPECT_EQ (-1, soma_string("-1,-2\n"));
+        EXPECT_EQ (-1, soma_string("1,-2\n"));
+        EXPECT_EQ (-1, soma_string("1\n,-2\n"));
+    }
 
-TEST (Ignorar, MaiorMil){
-    EXPECT_EQ (1, soma_string("1,2000\n"));
-    EXPECT_EQ (3, soma_string("3,5000\n"));
-    EXPECT_EQ (5, soma_string("3000,5\n"));
-    EXPECT_EQ (3, soma_string("3\n,5000\n"));
-    EXPECT_EQ (5, soma_string("3\n,2\n,5000\n"));
+    TEST (Ignorar, MaiorMil){
+        EXPECT_EQ (1, soma_string("1,2000\n"));
+        EXPECT_EQ (3, soma_string("3,5000\n"));
+        EXPECT_EQ (5, soma_string("3000,5\n"));
+        EXPECT_EQ (3, soma_string("3\n,5000\n"));
+        EXPECT_EQ (5, soma_string("3\n,2\n,5000\n"));
 
-}
+    }
 
-TEST (Invalido, SemFim){
-    EXPECT_EQ (-1, soma_string("1,2"));
-    EXPECT_EQ (-1, soma_string("1\n,2"));
-    EXPECT_EQ (-1, soma_string("1\n,2\n,3"));
-}
+    TEST (Invalido, SemFim){
+        EXPECT_EQ (-1, soma_string("1,2"));
+        EXPECT_EQ (-1, soma_string("1\n,2"));
+        EXPECT_EQ (-1, soma_string("1\n,2\n,3"));
+    }
 
 //testes falhos
 TEST (Invalido, Espaco){
     EXPECT_EQ (-1, soma_string("1,2 \n"));
 }
+
+TEST (Invalido, Virgulas){
+    EXPECT_EQ(-1, soma_string("1,,2\n"));
+}
+
+TEST (Invalido, DelimFalso){
+    EXPECT_EQ(-1, soma_string("1;2\n"));
+}
+
 
 TEST (Invalido, LimiteLinha){
     EXPECT_EQ (-1, soma_string("1,2,3,4\n"));
@@ -55,17 +64,8 @@ TEST (Invalido, LimiteLinha){
 TEST (Invalido, SingNum){
     EXPECT_EQ (-1, soma_string("1,\n"));
     EXPECT_EQ (-1, soma_string(",2\n"));
-}
-
-TEST (Invalido, Virgulas){
-    EXPECT_EQ(-1, soma_string("1,,2\n"));
     EXPECT_EQ (-1, soma_string(",\n"));
 }
-
-TEST (Invalido, DelimFalso){
-    EXPECT_EQ(-1, soma_string("1;2\n"));
-}
-
 
 
 int main(int argc, char **argv) {
--- Projeto 1/string_soma.c
+++ Projeto 1/string_soma.c
@@ -3,43 +3,59 @@
 #include <stdlib.h>
 #include "string_soma.h"
 
+
 bool IsNegative(int Num);
 bool HighValue(int Num);
-bool NoEnd(const char * string_entrada);
+bool NoEnd(char * );
+bool ManyDelimitador(const char *, char *);
 
 int soma_string(const char* string_entrada){
 
     //separa a string por delimitadores, a cada separaçao soma numero em soma.
     char *string, *entrada;
     int soma = 0;
+    char delimitadores[] = ",";
+
+    //transforma de const para char*
+    entrada = strdup(string_entrada);
 
     //Se possui nao possui  \n no final
-    if (NoEnd(string_entrada)) return -1;
+    if (NoEnd(entrada)) return -1;
 
     //Se possui espaço
     if (!(strpbrk(string_entrada, " ") == NULL)) return -1;
 
+    //Se possui delimitadores validos
+    if (strpbrk(string_entrada, delimitadores) == NULL) return -1;
 
-    //transforma de const para char*
-    entrada = strdup(string_entrada);
-    string = strtok(entrada, ",");
+    //se tem 2 delimitadores seguidos
+    if (ManyDelimitador(string_entrada, delimitadores)) return -1;
+
+
+    //divide string por delimitadores
+    string = strtok(entrada, delimitadores);
+
+    //puts(string);
+    //printf("%d\n", strlen(string));
 
 
     while(string != NULL){
 
         if (IsNegative(atoi(string))) return -1; //se x<0 para
 
-        if (!HighValue(atoi(string))){  //se x> 1000 ignora
+        if (!HighValue(atoi(string))){  //se x>1000 ignora
 
             soma += atoi(string);
         }
-        //puts(string);
-        string = strtok(NULL, ", ");
+
+        string = strtok(NULL, delimitadores);
 
     }
 
     return soma;
 }
+
+
 
 bool IsNegative(int Num){
 
@@ -51,7 +67,13 @@ bool HighValue(int Num){
     return Num > 1000;
 }
 
-bool NoEnd(const char * string_entrada){
+bool NoEnd(char * string){
 
-    return (strrchr(string_entrada, '\n') == NULL || !(strrchr(string_entrada, '\n') == (string_entrada + (strlen(string_entrada)-1))));
+    return (string[strlen(string)-1] != '\n');
+}
+
+bool ManyDelimitador(const char *string, char delimitadores[]){
+
+    return *(strpbrk(string, delimitadores)) == *(strpbrk(string, delimitadores)+1);
+    //return (strpbrk(string, delimitadores) == strpbrk(string+1, delimitadores));
 }
